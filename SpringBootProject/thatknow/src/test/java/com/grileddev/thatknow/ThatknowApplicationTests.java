d6f7c471755ce34fa3e023c656e38049343f6678
Shin <skynet3285@gmail.com>
2023-07-29 10:50:07 +0900

    Add SpringBoot Project

---
+++ SpringBootProject/thatknow/src/test/java/com/grileddev/thatknow/ThatknowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grileddev.thatknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThatknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
